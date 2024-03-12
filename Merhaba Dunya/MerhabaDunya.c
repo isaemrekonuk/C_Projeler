f560765e5e5b5c6dced64a635e1579ad1901be7a
isaemrekonuk <isaemrekonuk9@gmail.com>
2024-03-12 13:17:59 +0300

    Merhaba Dünya

---
+++ Merhaba Dunya/MerhabaDunya.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  //Kütüphane tanýmlama
+
+int main() 
+{
+    printf("Merhaba Dunya!"); //Ekrana yazdýrma komutu.    
+    
+    getchar(); //Konsolun ekranda kalmasýný saðlar.
+    
+    return 0;
+}
+    
