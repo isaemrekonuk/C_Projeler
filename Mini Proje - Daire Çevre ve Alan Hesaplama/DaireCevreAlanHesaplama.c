d1b298f8ba7a5fb605cb9fae827f4c4752bee5e9
isaemrekonuk <isaemrekonuk9@gmail.com>
2024-03-13 15:21:45 +0300

    Mini Proje - Dairenin Çevresi ve Alanını Hesaplama

--- Mini Proje - Daire Çevre ve Alan Hesaplama/DaireCevreAlanHesaplama.c
+++ Mini Proje - Daire Çevre ve Alan Hesaplama/DaireCevreAlanHesaplama.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <math.h> //Kütüphanemizi ekliyoruz.
 
 int main()
 {
 	//Değişkenlerimizi tanımlıyoruz.
-	float yaricap, cevre, alan, pi = 3.14;
+	float yaricap, cevre, alan;
 	
 	//Kullanıcıdan değer girmesini istiyoruz.
 	printf("Lutfen Dairenin Yaricap Uzunlugunu Giriniz..: ");
 	scanf("%f", &yaricap); //Girilen değeri ilgili değişkenimize atıyoruz.
 	
-	cevre = 2 * pi * yaricap; //Alınan değer ile ilgili işlemi yapıp sonucu değişkenimize atıyoruz.
+	cevre = 2 * M_PI * yaricap; //Alınan değer ile ilgili işlemi yapıp sonucu değişkenimize atıyoruz.
 	
-	alan = pi * (yaricap * yaricap); //Alınan değer ile ilgili işlemi yapıp sonucu değişkenimize atıyoruz.
+	alan = M_PI * (yaricap * yaricap); //Alınan değer ile ilgili işlemi yapıp sonucu değişkenimize atıyoruz.
 	
 	//Ekrana yazdırma işlemi yapıyoruz.
 	printf("Dairenin Cevresi..: %.2f \n", cevre);
